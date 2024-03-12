10a78ebcbd9054f6438d25a23dca1030af3460d6
Daniel Hilmer <daniel.hilmer@outlook.de>
2024-03-12 21:37:18 +0100

    Hinzufügen von CSS-Stil und JavaScript-Datei

---
+++ js/ue1.js
@@ -0,0 +1,134 @@
+// let myLet = "let"; // wollen wir verwenden! wenn die var überschreibbar sein soll
+// const myConst = "const"; // wollen wir verwenden! wenn die var nicht überschreibbar sein soll
+
+// console.log(myLet); // ich erwarte: let
+// console.log(myConst); // ich erwarte: const
+
+// // number
+// const myNumber = 5;
+// console.log(myNumber); // ich erwarte: 5
+
+// // string
+// const myString = "hallo ich bin ein string";
+// console.log(myString); // ich erwarte: hallo ich bin ein string
+
+// // boolean
+// const myBoolean = false;
+// console.log(myBoolean); // ich erwarte: true
+
+// // null
+// const myNull = null; // das macht der coderIn
+// console.log(myNull); // ich erwarte: null
+
+// // undefined
+// const myUndefined = undefined; // das macht JS
+// console.log(myUndefined); // ich erwarte: undefined
+
+// const test = "";
+// console.log(test); // ich erwarte: ""
+
+// const arrName = ["Max", true, 123, [true, 4, "hallo"]]; // 0, 1, 2
+// console.log(arrName);
+// console.log(arrName[3]);
+
+// console.log(arrName[3][2], arrName[1], arrName[2]);
+
+// const myObj = {
+//   name: "Max",
+//   age: 24,
+//   isLoggedIn: true,
+//   hobby: ["sport", "freunde"],
+// };
+// console.log(myObj.hobby[0]);
+// console.log(myObj.isLoggedIn);
+
+// const myArr = [
+//   { name: "Max", age: 24, hobby: ["sport", "freunde"] },
+//   { name: "Tim", age: 30, hobby: ["sport"] },
+//   { name: "Lea", age: 50, hobby: ["freunde"] },
+//   { name: "Lisa", age: 20, hobby: ["freunde"] },
+// ];
+
+// console.log(myArr[0].hobby);
+
+// console.log(myArr);
+
+// let a = 17;
+// console.log(a);
+
+// const b = 3;
+// console.log(b);
+
+// const c = a ** b; // hoch
+
+// console.log(c);
+
+// const d = a % b;
+// console.log(d);
+
+// a++; // a = a + 1
+
+// console.log(a);
+
+// a++;
+
+// console.log(a);
+
+// a--; // a = a - 1
+
+// console.log(a);
+
+// console.log(a--);
+// console.log(a);
+
+// const a = "1";
+// const b = 1;
+
+// console.log(b - +a);
+
+// function calc(num1, num2) {
+//   console.log("num1: " + num1);
+//   console.log("num2: " + num2);
+//   const ergebnis = num1 + num2;
+//   console.log("Num3 in der Funktion: " + ergebnis);
+
+//   return num1 + num2;
+// }
+
+// const ausgabe = calc(5, 9);
+
+// console.log("Ausgabe: " + ausgabe);
+
+// const x = 1;
+// const y = 2;
+
+// const calc = (num1, num2) => {
+//   return num1 + num2;
+// };
+
+// const ausgabe = calc(x, y);
+// console.log(ausgabe);
+
+let a;
+
+a = 1;
+
+// const myFunc = (name) => {
+//   console.log("Hallo " + name);
+// };
+
+// myFunc("Daniel");
+
+// console.log(1);
+
+// setTimeout(() => {
+//   console.log(2);
+// }, 2000);
+
+// console.log(3);
+
+// const arr = ["Max", "Hallo", true, 13];
+
+// arr.forEach((param) => {
+//   console.log(param);
+// });
