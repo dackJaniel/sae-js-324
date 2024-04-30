6556ace08feb282c2d6f52ccf4117a6bd164325e
Daniel Hilmer <daniel.hilmer@outlook.de>
2024-04-30 21:21:33 +0200

    erstes vite projekt

---
+++ Library-API-UE1/app/src/js/calc.js
@@ -0,0 +1,13 @@
+export function add(num1, num2) {
+  return num1 + num2;
+}
+
+export function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+export class Calc {}
+
+export const pi = 3.14159;
+
+// export { add, subtract };
---
+++ Library-API-UE1/app/src/js/main.js
@@ -0,0 +1,5 @@
+import { add, subtract } from "./calc";
+
+console.log(add(1, 2)); // 3
+console.log(subtract(2, 1)); // 1
+console.log(pi); // 3.14159
